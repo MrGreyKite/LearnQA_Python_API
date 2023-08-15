dece7e059aabad6d030c77cc9355fc12d09396e6
MrGreyKite <nolde3@yandex.ru>
2023-08-15 17:43:15 +0300

    ДЗ4, задание 16

--- tests/test_user_info.py
+++ tests/test_user_info.py
@@ -14,7 +14,7 @@ class TestUserInfo(BaseCase):
         Assertions.assert_json_doesnt_have_key(response, "firstName")
         Assertions.assert_json_doesnt_have_key(response, "lastName")
 
-    def test_get_user_inf0_as_same_user(self):
+    def test_get_user_info_as_same_user(self):
         data = {
             "email": "vinkotov@example.com",
             "password": "1234"
@@ -30,3 +30,22 @@ class TestUserInfo(BaseCase):
                                        cookies={"auth_sid": auth_sid}
                                        )
         Assertions.assert_json_has_keys(response_info, ["username", "email", "firstName", "lastName"])
+
+    def test_get_user_info_as_another_user(self):
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+        }
+        response_for_auth = MyRequests.post(url=self.url_login, data=data)
+
+        auth_sid = self.get_cookie(response_for_auth, "auth_sid")
+        csrf_token = self.get_header(response_for_auth, "x-csrf-token")
+
+        response_info = MyRequests.get(url=self.url_user_by_id_pattern + "1",
+                                       headers={"x-csrf-token": csrf_token},
+                                       cookies={"auth_sid": auth_sid}
+                                       )
+        Assertions.assert_json_has_key(response_info, "username")
+        Assertions.assert_json_doesnt_have_key(response_info, "email")
+        Assertions.assert_json_doesnt_have_key(response_info, "firstName")
+        Assertions.assert_json_doesnt_have_key(response_info, "lastName")
