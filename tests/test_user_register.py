f772420912bd8ac95592bdac93369b834b17ca22
MrGreyKite <nolde3@yandex.ru>
2023-08-15 17:17:51 +0300

    ДЗ4, задание 15

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,3 +1,5 @@
+import pytest
+
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
@@ -22,3 +24,34 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code_is(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{self.existing_email}' already exists", \
             f"Unexpected response content {response.content}"
+
+    def test_create_user_invalid_email(self):
+        data = self.prepare_registration_data('invalid_mail.ru')
+
+        response = MyRequests.post(url=self.url_create_user, data=data)
+        print(response.content)
+        Assertions.assert_status_code_is(response, 400)
+        assert response.content.decode("utf-8") == "Invalid email format"
+
+    @pytest.mark.parametrize('empty_field', ['username', 'firstName', 'lastName', 'email', 'password'])
+    def test_create_user_missing_field(self, empty_field):
+        data = self.prepare_registration_data()
+
+        data.pop(empty_field)
+        response = MyRequests.post(url=self.url_create_user, data=data)
+        Assertions.assert_status_code_is(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {empty_field}"
+
+    def test_create_user_too_short_name(self):
+        data = self.prepare_registration_data()
+        data['username'] = "t"
+        response = MyRequests.post(url=self.url_create_user, data=data)
+        Assertions.assert_status_code_is(response, 400)
+        assert response.content.decode("utf-8") == "The value of \'username\' field is too short"
+
+    def test_create_user_too_long_name(self):
+        data = self.prepare_registration_data()
+        data['username'] = "test_user" * 50
+        response = MyRequests.post(url=self.url_create_user, data=data)
+        Assertions.assert_status_code_is(response, 400)
+        assert response.content.decode("utf-8") == "The value of \'username\' field is too long"
